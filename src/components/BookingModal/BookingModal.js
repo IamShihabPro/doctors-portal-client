c7516eabfffc5a43accaeaac6c881e8612d12ad3
IamShihabPro <mshihab.pro@gmail.com>
2023-07-05 03:46:06 +0600

    doctors portal client

--- src/components/BookingModal/BookingModal.js
+++ src/components/BookingModal/BookingModal.js
@@ -1,6 +1,9 @@
+import { format } from 'date-fns/esm';
 import React from 'react';
 
-const BookingModal = ({treatment}) => {
+const BookingModal = ({treatment, selectedDate}) => {
+    const {name, slots} = treatment
+    const date = format(selectedDate, 'PP')
     return (
         <>
 
@@ -9,17 +12,34 @@ const BookingModal = ({treatment}) => {
             <div className="modal">
             <div className="modal-box">
                 <div className='flex justify-between items-center'>
-                    <h3 className="font-bold text-lg">{treatment.name}</h3>
+                    <h3 className="font-bold text-lg">{name}</h3>
 
                     <div className="modal-action">
-                    <label htmlFor="my_modal_6" className="btn btn-primary btn-xs text-white">X</label>
+                    <label htmlFor="my_modal_6" className="btn btn-secondary btn-xs text-white rounded-full">X</label>
                     </div>
 
                 </div>
-                <p className="py-4">This modal works with a hidden checkbox!</p>
+               
                 {/* <div className="modal-action">
                 <label htmlFor="my_modal_6" className="btn">Close!</label>
                 </div> */}
+
+                <form className='grid grid-cols-1 gap-3 mt-10'> 
+                    <input type="text" value={date} disabled className="input input-bordered input-accent w-full " />
+
+                    <select className="select select-accent w-full">
+                        {
+                            slots.map(slot =>  <option value={slot}> {slot}</option>)
+                        }
+                       
+                    </select>
+
+                    <input type="text" placeholder="Type here" className="input input-bordered input-accent w-full " />
+                    <input type="text" placeholder="Type here" className="input input-bordered input-accent w-full " />
+                    <input type="text" placeholder="Type here" className="input input-bordered input-accent w-full " />
+                    <br />
+                    <input type="submit" value='Submit' className='btn btn-secondary w-full text-white' />
+                </form>
             </div>
             </div>
             
